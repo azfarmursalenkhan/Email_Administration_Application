7480e07b3265e3211aaae5f440d662bd12df45c4
azfarmursalenkhan <azfarmursalenkhan@yahoo.com>
2021-01-10 18:02:01 +0100

    The project for JAva Email Application

---
+++ src/com/company/Email.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+
+    //constructor to recveive the first name and last name
+
+
+    // ask for the department.
+
+
+    // generate a random password
+
+
+
+    //set athe mailbox capacity.
+
+    //set the alternate email
+
+    //change teh password.
+
+
+
+
+}
